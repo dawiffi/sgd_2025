3c2d1e4f9c178795f07f30bd1b35f099fb9d1473
dawiffi <dawid.frontczak11@gmail.com>
2025-05-23 21:22:19 +0200

    catanimation.py done

--- catanimation.py
+++ catanimation.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 
-FPS = 30 # frames per second setting
+FPS = 60 # frames per second setting
 fpsClock = pygame.time.Clock()
 
 # set up the window
@@ -12,9 +12,13 @@ pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
 catImg = pygame.image.load('cat.png')
+catImg2 = pygame.image.load('cat2.png')
 catx = 10
 caty = 10
+catx2 = 275
+caty2 = 225
 direction = 'right'
+direction2 = 'up'
 
 while True: # the main game loop
     DISPLAYSURF.fill(WHITE)
@@ -37,6 +41,25 @@ while True: # the main game loop
             direction = 'right'
 
     DISPLAYSURF.blit(catImg, (catx, caty))
+
+    if direction2 == 'up':
+        caty2 -= 5
+        if caty2 == 10:
+            direction2 = 'left'
+    elif direction2 == 'left':
+        catx2 -= 5
+        if catx2 == 10:
+            direction2 = 'down'
+    elif direction2 == 'down':
+        caty2 += 5
+        if caty2 == 220:
+            direction2 = 'right'
+    elif direction2 == 'right':
+        catx2 += 5
+        if catx2 == 280:
+            direction2 = 'up'
+    
+    DISPLAYSURF.blit(catImg2, (catx2, caty2))
 
     for event in pygame.event.get():
         if event.type == QUIT:
